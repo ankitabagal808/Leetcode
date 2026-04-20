a7fe705439560eaa3ce927defe608980b977e071
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2026-04-20 12:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (24.44%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+class MyQueue 
+{
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public MyQueue() 
+    {
+        
+    }
+    
+    public void push(int x) 
+    {
+        s1.push(x);
+    }
+    
+    private void transfer()
+    {
+        if (s2.isEmpty()) 
+        {
+            while (!s1.isEmpty()) 
+            {
+                s2.push(s1.pop());
+            }
+        }
+    }
+    
+    public int pop() 
+    {
+        transfer();
+        return s2.pop();
+    }
+    
+    public int peek() 
+    {
+        transfer();
+        return s2.peek();
+    }
+    
+    public boolean empty() 
+    {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
