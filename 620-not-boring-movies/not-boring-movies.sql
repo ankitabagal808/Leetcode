745cee2c0e271c9b7ecc1267e5f944437a50a3d4
ankitabagal808 <103379119+ankitabagal808@users.noreply.github.com>
2026-07-21 17:13:21 +0530

    Time: 459 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE id % 2 = 1
-  AND description != 'boring'
-ORDER BY rating DESC;
+-- Write your PostgreSQL query statement below
+select * 
+from cinema 
+where id%2=1
+and description!='boring'
+order by rating desc;
